9ddbdbe154db888b1468dc572d50148edd506234
megan.a.chan <megan.a.chan@avanade.com>
2025-01-20 14:40:27 -0800

    added profiles for env specific configs

--- spring-boot-config/src/main/java/com/megandemo/spring_boot_config/GreetingController.java
+++ spring-boot-config/src/main/java/com/megandemo/spring_boot_config/GreetingController.java
@@ -2,6 +2,7 @@ package com.megandemo.spring_boot_config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,8 @@ public class GreetingController {
     @Autowired
     private DbSettings dbSettings;
 
+    @Autowired
+    private Environment env;
 
     @GetMapping("/greeting")
     public String hello() {
@@ -46,5 +49,10 @@ public class GreetingController {
     @GetMapping("/db")
     public String db() {
         return dbSettings.getConnection() + ", " + dbSettings.getHost() + ", " + dbSettings.getPort();
+    }
+
+    @GetMapping("/envdetails")
+    public String envDetails() {
+        return env.toString();
     }
 }
